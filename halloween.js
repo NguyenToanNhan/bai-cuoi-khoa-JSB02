cd7d9f2f68a1d66269b6561e7bd1b307cc1dc7d7
NguyenToanNhan <toannhandockcom@gmail.com>
2023-11-24 22:03:39 +0700

    first commit

---
+++ halloween.js
@@ -0,0 +1,51 @@
+var click1 = document.getElementById('click1');
+
+click1.addEventListener('click', function(){
+    document.getElementById('article-content').innerHTML =  '<h4>When my sister Betsy and I were kids, our family lived for awhile in a charming old farmhouse. We loved exploring its dusty corners and climbing the apple tree in the backyard. But our favorite thing was the ghost. We called her Mother, because she seemed so kind and nurturing. Some mornings Betsy and I would wake up, and on each of our nightstands, we’d find a cup that hadn’t been there the night before. Mother had left them there, worried that we’d get thirsty during the night. She just wanted to take care of us. Among the homes’ original furnishings was an antique wooden chair which we kept against the back wall of the living room. Whenever we were preoccupied, watching TV or playing a game, Mother would inch that chair forward, across the room, toward us. Sometimes she’d manage to move it all the way to the centre of the room. We always felt sad putting it back against the wall. Mother just wanted to be near us. Years later, long after we’d moved out, I found an old newspaper article about the farmhouse’s original occupant, a widow. She’d murdered her two children by giving them each a cup of poisoned milk before bed. Then she hung herself. The article included a photo of the farmhouse’s living room, with a woman’s body hanging from a beam. Beneath her, knocked over, was that old wooden chair, placed exactly in the center of the room.</h4>'+
+
+    '<h4>…Did it get colder in here, or is it just me?</h4>'+
+                                                            '<div class="mobs">'+
+                                                            '</div>' ;
+})
+var click2 = document.getElementById('click2');
+
+click2.addEventListener('click', function(){
+    document.getElementById('article-content').innerHTML =  '<h4>Mommy told me never to go in the basement, but I wanted to see what was making that noise. It kind of sounded like a puppy, and I wanted to see the puppy, so I opened the basement door and tiptoed down a bit. I didn’t see a puppy, and then Mommy yanked me out of the basement and yelled at me. Mommy had never yelled at me before, and it made me sad and I cried. Then Mommy told me never to go into the basement again, and she gave me a cookie. That made me feel better, so I didn’t ask her why the boy in the basement was making noises like a puppy, or why he had no hands or feet.</h4>'+
+                                                            '<div class="mobs">'+
+                                                            '</div>' ;
+})
+var click3 = document.getElementById('click3');
+
+click3.addEventListener('click', function(){
+    document.getElementById('article-content').innerHTML =  '<h4>He got to the graveyard and as expected it was dead silent apart from a few howls that were howling on the trees. Martin cared less about the, howls and the rest of the graves ,he only wanted to verify if she was alive or dead .He got to where the grave was, he looked and there she was, still looking at him with those sad eyes ,it was as if she was speaking to him as if she was trying to say something to him .Martin looked at her keenly and saw she had goose bumps. He got into the grave and looked at her closely she was still looking at him, her eyes were so full of life and though she was dead she was so beautiful ,Martin opened the entire casket and looked at her body ,she was gorgeous ,he looked up , the moon was still half way out ,he could still manage to carry her home .</h4>'+
+                                                            '<div class="mobs">'+
+                                                            '</div>' ;
+                                                        })
+var click4 = document.getElementById('click4');
+                                                        
+click4.addEventListener('click', function(){
+    document.getElementById('article-content').innerHTML =  '<h4>Last night a friend rushed me out of the house to catch the opening act at a local bar’s music night. After a few drinks I realized my phone wasn’t in my pocket. I checked the table we were sitting at, the bar, the bathrooms, and after no luck I used my friend’s phone to call mine. After two rings someone answered, gave out a low raspy giggle, and hung up. They didn’t answer again. I eventually gave it up as a lost cause and headed home. I found my phone laying on my nightstand, right where I left it.</h4>'+
+                                                            '<div class="mobs">'+
+                                                            '</div>' ;
+                                                        })
+var click5 = document.getElementById('click5');
+                                                        
+click4.addEventListener('click', function(){
+    document.getElementById('article-content').innerHTML =  '<h4>Jayne laughed and walked towards the men, gun up and ready to shoot. “Back off gentlemen and let us take the child.  Honestly imagine if someone had taken you from your mommies when you were this age. Scary scary stuff if you ask me. And look at you. You’re all so young with a full life ahead of you. You need to go to graduate school or join the Peace Corps or do something with your lives besides taking money to steal children from those who aren’t like you. Make something of yourselves.  Learn to hula dance or tap dance or ballroom dance. Or you could just go to sleep. Go to sleep and dream of warm fires and gentle ocean breezes and the wind through the palm trees and white sands and ….</h4>'+
+                                                            '<div class="mobs">'+
+                                                            '</div>' ;
+                                                        })
+var click6 = document.getElementById('click6');
+                                                        
+click6.addEventListener('click', function(){
+    document.getElementById('article-content').innerHTML =  '<h4>It is a northern country; they have cold weather, they have cold hearts.Cold; tempest; wild beasts in the forest. It is a hard life. Their houses are built of logs, dark and smoky within. There will be a crude icon of the virgin behind a guttering candle, the leg of a pig hung up to cure, a string of drying mushrooms. A bed, a stool, a table. Harsh, brief, poor lives.To these upland woodsmen, the Devil is as reals as you or I. More so; they have not seen us nor even know that we exist, but the Devil they glimpse often in the graveyards, those bleak and touching townships of the dead where the graves are marked with portraits of the deceased in the naif style and there are no flowers to put in front of them, no flowers grow there, so they put out small votive offerings, little loaves, sometimes a cake that the bears come lumbering from the margins of the forests to snatch away. At midnight, especially on Walpurgisnacht, the Devil holds picnics in the graveyards and invites the witches; then they dig up fresh corpses, and eat them. Anyone will tell you that.Wreaths of garlic on the doors keep out the vampires. A blue-eyed child born feet first on the night of St. John’s Eve will have second sight. When they discover a witch – some old woman whose cheeses ripen when her neighbours’ do not, another old woman whose black cat, oh, sinister! follows her about all the time, they strip the crone, search for her marks, for the supernumerary nipple her familiar sucks. They soon find it. Then they stone her to death.</h4>'+
+                                                            '<div class="mobs">'+
+                                                            '</div>' ;
+                                                        })
+var click7 = document.getElementById('click7');
+                                                        
+click7.addEventListener('click', function(){
+    document.getElementById('article-content').innerHTML =  '<h4>Ma còn được gọi là hồn ma, là một khái niệm theo quan niệm dân gian để chỉ hồn của người chết (hoặc các sinh vật đã chết khác như động vật) xuất hiện ở thế giới của người đang sống. Không có bằng chứng khoa học nào cho thấy hồn ma tồn tại. Sự tồn tại của hồn ma được coi là không thể phủ chứng được, không thể kiểm chứng bằng các phương pháp khoa học. Do đó các nghiên cứu về hồn ma, hay săn ma bị liệt vào phạm trù ngụy khoa học.</h4>'+
+                                                            '<div class="mobs">'+
+                                                            '</div>' ;
+                                                        })
